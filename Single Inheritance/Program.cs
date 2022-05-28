da2aae8498b4582442c0ed7258d9b0019339eeac
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-28 05:30:50 +0530

    Add project files.

---
+++ Single Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+    class Program:parent
+    {
+      
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.show();
+            obj.print();
+          
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Single Inheritance/parent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+    class parent
+    {
+      
+       
+    void name()
+        {
+            Console.WriteLine("hiii");
+        }
+        protected   void show()
+        {
+            Console.WriteLine("Hello My Name Akshay Saini");
+        }
+        protected  void print()
+        {
+           this. show();
+            Console.WriteLine("Hello");
+        }
+    }
+}
